1e0f4ca997dd574482540e2ab18eccc5c4ca1906
Deftu <deftudev@gmail.com>
2024-07-02 06:44:34 +0200

    Update TestModDearImGuiEntrypoint.java

--- src/testMod/java/testmod/imgui/TestModDearImGuiEntrypoint.java
+++ src/testMod/java/testmod/imgui/TestModDearImGuiEntrypoint.java
@@ -2,12 +2,11 @@ package testmod.imgui;
 
 import dev.deftu.imgui.DearImGuiEntrypoint;
 import dev.deftu.imgui.ImGuiRenderer;
-import net.fabricmc.loader.api.FabricLoader;
 
 public class TestModDearImGuiEntrypoint implements DearImGuiEntrypoint {
 
     public ImGuiRenderer createRenderer() {
-        return FabricLoader.getInstance().isDevelopmentEnvironment() ? TestModImGuiRenderer.INSTANCE : null;
+        return TestModImGuiRenderer.INSTANCE;
     }
 
 }
